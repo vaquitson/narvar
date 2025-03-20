319f9597eba1d12926cafa398ebb9e2f2496933a
vaquitson <vaquitson@gmail.com>
2025-03-20 16:37:05 -0300

    bsaic proyect structure

---
+++ build/CMakeFiles/narvar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/narvar.dir/src/main.c.o"
+  "CMakeFiles/narvar.dir/src/main.c.o.d"
+  "narvar"
+  "narvar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/narvar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/narvar.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for narvar.
+# This may be replaced when dependencies are built.
